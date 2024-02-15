7d3f7e3a85adb9aabe8a7832ab07cb79891badbe
木村 敏之 041674 <041674>
2024-02-15 13:01:06 +0900

    login完了

--- euc_etc_id.py
+++ euc_etc_id.py
@@ -1,25 +1,82 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from config import BINARY_LOCATION,MSEDGE_DRIVER, USER_NAME, USER_PASSWORD
 
 # Edgeのオプション設定
 edge_options = Options()
-edge_options.use_chromium =True
-edge_options.add_experimental_option('ieMode',True)
-edge_options.binary_location = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
+edge_options.use_chromium = True  # ChromiumベースのEdgeを使用することを指定
+edge_options.binary_location = BINARY_LOCATION
 
 # Edge WebDriverのパスを指定する
-service = Service(executable_path="\\cpiweb\\www\\4930\\ソフトウェア\\MicrosoftEdge\\WebDriver\\120.0.2210.121\\msedgedriver.exe")
+service = Service(executable_path=MSEDGE_DRIVER)
 
 # WebDriverのインスタンスを作成する
 driver = webdriver.Edge(service=service, options=edge_options)
+username = USER_NAME
+password = USER_PASSWORD
 
-# Googleのホームページを開く
-driver.get("https://www.google.com")
+def login_kikan_with_seleniun(driver,username,password):
+    """基幹システムにログインする関数
 
-# ウェブページのタイトルを出力する（確認用）
-print(driver.title)
+    Args:
+        driver: Selenium WebDriverのインスタンス
+        username: ユーザー名
+        password: パスワード
+
+    Returns:
+        ログイン後のWebDriverのインスタンス
+    """    
+    # 新基幹のページを開く
+    driver.get("http://core-apserver.prec.canon.co.jp/ZZ_CPIPortal/page/myLogin.jsp")
+
+    # ユーザー入力欄の要素を取得
+    user_element = driver.find_element(By.ID, 'user')
+    # ユーザー入力欄に文字列を入力
+    user_element.send_keys(username)
+
+    # パスワード入力欄の要素を取得
+    user_element = driver.find_element(By.ID, 'pass')
+    # パスワード入力欄に文字列を入力
+    user_element.send_keys(password)
+
+    # ボタンの要素を取得
+    submit_element = driver.find_element(By.ID, "submit")
+    # ボタンをクリック
+    submit_element.click()
+
+    return driver
+login_kikan_with_seleniun(driver,username,password)
+
+#!
+# 別ウィンドウを開く前に現在ウィンドウのハンドルを取得
+current_window_handle = driver.current_window_handle
+print(current_window_handle)
+link_element = driver.find_element(By.ID, 'sidA_LNK_05')
+link_element.click()
+# 別ウィンドウが開くまで待つ
+WebDriverWait(driver, 10).until(
+    EC.number_of_windows_to_be(2)
+)
+
+# 別ウィンドウのハンドルを取得
+new_window_handle = [
+    handle for handle in driver.window_handles if handle != current_window_handle
+][0]
+print(new_window_handle)
+# 別ウィンドウに切り替え
+driver.switch_to.window(new_window_handle)
+
+# 別ウィンドウで要素をクリック
+element_element = driver.find_element(By.ID, 'a_lnk_01_item')
+element_element.click()
+sleep(10)
+
 
 # ブラウザを閉じる
 driver.quit()
