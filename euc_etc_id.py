962b5a489b29a67e7be57f6a03064054cb798d8e
phonon-py <phn.19890918@gmail.com>
2024-02-19 12:52:19 +0900

    一度全てを要へ変更する

--- euc_etc_id.py
+++ euc_etc_id.py
@@ -6,7 +6,8 @@ from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from config import BINARY_LOCATION,MSEDGE_DRIVER, USER_NAME, USER_PASSWORD,KIKAN_URL
+import pandas as pd
+from config import BINARY_LOCATION,MSEDGE_DRIVER, USER_NAME, USER_PASSWORD,KIKAN_URL, DF_PATH
 
 # Edgeのオプション設定
 edge_options = Options()
@@ -71,10 +72,49 @@ new_window_handle = [
 # 別ウィンドウに切り替え
 driver.switch_to.window(new_window_handle)
 
-# 別ウィンドウで要素をクリック
-element_element = driver.find_element(By.ID, 'a_lnk_01_item')
-element_element.click()
+# 棚卸し開始をクリック
+inventory_start_element = driver.find_element(By.ID, 'a_lnk_01_item')
+inventory_start_element.click()
 
+# 詳細をクリック
+detail_element = driver.find_element(By.ID, 'sidAPP_BUTTON')
+detail_element.click()
+
+# 要素を取得
+element = driver.find_element(By.ID, "g[0].tana_button_item")
+# 要素をクリック
+element.click()
+
+# ここでデータフレームの読込み
+df = pd.read_excel(DF_PATH)
+
+#! 全部要へ変更する
+
+#! テーブル要素を取得
+table = driver.find_element(By.ID, "TH_g")
+print(table)
+
+#! テーブルID、TH_gの中からpandasのユーザー名id sidO_USER_NAMEのテキストとid sidO_KENGEN_NAMEが一致する
+
+#! g[0].tana_button_itemをfor分で回す
+
+# 最大ループ回数
+MAX_LOOP = 10
+# ループ処理
+for i in range(MAX_LOOP):
+    try:
+        # 要素を取得
+        element = driver.find_element(By.ID, f"g[{i}].tana_button_item")
+        
+        # 要素をクリック
+        element.click()
+
+        # ブラウザバック
+        driver.back()
+        
+    except:
+        # 要素が存在しない場合はループを終了
+        break
 
 # ブラウザを閉じる
 driver.quit()
--- wiki.md
+++ wiki.md
@@ -4,3 +4,8 @@
 <SPAN id=export_b_item title="" class=action><INPUT onclick="setupEvent(event);if(!checkAndResetBlurCheckResult()){return false;}event_onClick_withIFrame('export_b','export_bAction','',0)" onkeyup="setupEvent(event);cursor_onKeyUp('EXPORT_B')" onfocus="setupEvent(event);event_onFocus('action_export_b')" id=sidEXPORT_B class=IoAction onkeydown="setupEvent(event);return event_onKeyDown4Item('EXPORT_B', '')" type=button value=ＩＤリスト出力 name=action_export_b> 
 </SPAN>
 ```
+
+## IEモードでHTMLの要素を取得したいとき
+```
+"C:\Windows\System32\F12\IEChooser.exe" 
+```
